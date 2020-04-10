99414c31e0053f8729598aac4db2c342f57152a2
Yocote0111 <yokota-masanori@zipc.com>
2020-04-10 12:38:37 +0900

    init

---
+++ snippet.js
@@ -0,0 +1,4 @@
+// add jquery feature to the page.
+let s = document.createElement('script');
+s.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+document.getElementsByTagName('head')[0].append(s);
