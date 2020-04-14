dd751043082383cf785a5673e87bbcc0d6cdb6e0
Yocote0111 <yokota-masanori@zipc.com>
2020-04-14 22:50:04 +0900

    schema study

---
+++ schema/README.md
@@ -0,0 +1,38 @@
+# JSON & YAML schema
+
+## JSON
+Ref: [https://code.visualstudio.com/docs/languages/json]  
+json ファイルで, schema を "$schema" プロパティを指定することでバリデーション可能.  
+json-schema など, package.json など, 定型のJSONファイルを書く際に有用.  
+
+```JSON
+{
+    "$schema": "http://json-schema.org/draft-07/schema",
+    "type": "object",
+    "properties": {
+        // ..
+    }
+}
+```
+### schema list
+Ref: [http://schemastore.org/json/]
+json-schema: "http://json-schema.org/draft-07/schema"  
+package.json: "http://json.schemastore.org/package"  
+
+## YAML
+Ref: [https://marketplace.visualstudio.com/items?itemName=redhat.vscode-yaml]  
+settings.json で "yaml.schemas" プロパティを指定することでバリデーション可能.
+
+```JSON
+{
+    "yaml.schemas": {
+        "http://json.schemastore.org/swagger-2.0": ["*swagger.yaml", "*swagger.yml"],
+        "file:///C:/Users/yokota-masanori.CATS/git/node/sample.json": ["*.s.yaml","*.s.yml"],
+        "https://raw.githubusercontent.com/docker/compose/master/compose/config/config_schema_v3.8.json": ["*docker-compose.yaml", "*docker-compose.yml"]
+    }
+}
+```
+### schema list
+
+swagger: "http://json.schemastore.org/swagger-2.0"  
+docker-compose: "https://raw.githubusercontent.com/docker/compose/master/compose/config/config_schema_v3.8.json"
--- snippet.js
+++ snippet.js
@@ -1,4 +1,43 @@
-// add jquery feature to the page.
-let s = document.createElement('script');
-s.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
-document.getElementsByTagName('head')[0].append(s);
+// include
+function include(src){ 
+  let s = document.createElement('script');
+  s.setAttribute('src', src);
+  document.getElementsByTagName('head')[0].append(s); 
+}
+
+// jquery feature
+include('https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+
+// show image in console
+console.image = function (img, scale=100){
+  console.log('%c ', `padding: ${Math.min(50, img.height/img.width*scale)}px ${scale}px; background:url(${img.src}) no-repeat center; background-size: contain`);
+  console.log(img);
+}
+
+// console each element
+console.each = function($e, f){ 
+  f = f || console.log; 
+  $e.each((i,e)=>f(e)); 
+}
+
+// Download file
+Blob.prototype.download = function(name='download.txt'){
+  let a = document.createElement('a');
+  a.href = URL.createObjectURL(this);
+  a.download = this.name || name;
+  a.style.display = 'none';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+
+console.image = function(img){
+  console.log('%c', `font-size:${img.height}; background-image:url(${img.src}); no-repeat;`);
+}
+
+let i1 = 
+
+
+whideyuki@jari.or.jp
+
